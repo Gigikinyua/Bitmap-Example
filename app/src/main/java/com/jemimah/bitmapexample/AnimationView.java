bdbcc95bdad21ee55f74e956e0af4c304f9e5a28
Jemimah <jemkiyua@gmail.com>
2023-02-22 20:49:36 +0300

    swipe working

--- app/src/main/java/com/jemimah/bitmapexample/AnimationView.java
+++ app/src/main/java/com/jemimah/bitmapexample/AnimationView.java
@@ -35,6 +35,8 @@ public class AnimationView extends View {
     float[] pos;
     float[] tan;
 
+    int bitmapIndex=0;
+
     boolean swipe = false;
 
     GestureDetector gestureDetector;
@@ -67,13 +69,13 @@ public class AnimationView extends View {
         initMyView();
     }
 
-    public void initMyView(){
+    public void initMyView() {
         paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
 
         bm = BitmapFactory.decodeResource(getResources(), R.drawable.woman);
-        bm_offsetX = bm.getWidth()/2;
-        bm_offsetY = bm.getHeight()/2;
+        bm_offsetX = bm.getWidth() / 2;
+        bm_offsetY = bm.getHeight() / 2;
 
         animPath = new Path();
 
@@ -110,43 +112,87 @@ public class AnimationView extends View {
 
         canvas.drawPath(animPath, paint);
 
-        Toast.makeText(context, "PathLength: " + pathLength/4, Toast.LENGTH_SHORT).show();
-
-        float startPoint = pathLength / 4;
+        float interval = pathLength / 3;
 
 
 
+        float pos2 = distance + interval;
+        float pos3 = pos2 + interval;
 
-        if (!swipe) {
-            canvas.drawBitmap(bm, 0, 0,  null);
-        } else {
 
-            if (distance < pathLength) {
-                pathMeasure.getPosTan(distance, pos, tan);
+        for ( int j=0; j < 3; j++) {
+            bitmapIndex =j;
 
-                matrix.reset();
-                float degrees = (float) (Math.atan2(tan[1], tan[0]) * 180.0 / Math.PI);
-                matrix.postRotate(degrees, bm_offsetX, bm_offsetY);
-                matrix.setTranslate(pos[0] - bm_offsetX, pos[1] - bm_offsetY);
+            if (!swipe) {
 
-                canvas.drawBitmap(bm, matrix, null);
-
-                distance += step;
-
+                drawBitmap(j, distance, pos2, pos3, canvas);
             } else {
-                distance = 0;
+
+                float endpoint;
+                if (j == 0) {
+                    endpoint = 0 + interval;
+                    if (distance <= endpoint) {
+                        drawBitmap(j, distance, pos2, pos3, canvas);
+
+                        distance += step;
+                    } else {
+                        drawBitmap(j, distance, pos2, pos3, canvas);
+                        swipe= false;
+                    }
+                } else {
+                    drawBitmap(j, distance, pos2, pos3, canvas);
+                }
             }
+
         }
 
+
         invalidate();
+
+
+
+    }
+
+    public void drawBitmap(int view, float pos1, float pos2, float pos3, Canvas canvas) {
+
+        if (view == 0) {
+            pathMeasure.getPosTan(pos1, pos, tan);
+
+            matrix.reset();
+            float degrees = (float) (Math.atan2(tan[1], tan[0]) * 180.0 / Math.PI);
+            matrix.postRotate(degrees, bm_offsetX, bm_offsetY);
+            matrix.setTranslate(pos[0] - bm_offsetX, pos[1] - bm_offsetY);
+
+            canvas.drawBitmap(bm, matrix, null);
+        } else if (view == 1){
+            pathMeasure.getPosTan(pos2, pos, tan);
+
+            matrix.reset();
+            float degrees = (float) (Math.atan2(tan[1], tan[0]) * 180.0 / Math.PI);
+            matrix.postRotate(degrees, bm_offsetX, bm_offsetY);
+            matrix.setTranslate(pos[0] - bm_offsetX, pos[1] - bm_offsetY);
+
+            canvas.drawBitmap(bm, matrix, null);
+        } else if (view == 2){
+            pathMeasure.getPosTan(pos3, pos, tan);
+
+            matrix.reset();
+            float degrees = (float) (Math.atan2(tan[1], tan[0]) * 180.0 / Math.PI);
+            matrix.postRotate(degrees, bm_offsetX, bm_offsetY);
+            matrix.setTranslate(pos[0] - bm_offsetX, pos[1] - bm_offsetY);
+
+            canvas.drawBitmap(bm, matrix, null);
+        }
     }
 
     void forward() {
+        distance=0;
         Toast.makeText(context, "swiped forward", Toast.LENGTH_SHORT).show();
         this.postInvalidate();
     }
 
     void backward() {
+        distance=0;
         Toast.makeText(context, "swiped back", Toast.LENGTH_SHORT).show();
         this.postInvalidate();
     }
@@ -226,6 +272,14 @@ public class AnimationView extends View {
         Log.d(TAG, "onSwipeBottom: Move forward");
         swipe = true;
         forward();
+    }
+
+    static class CustomViewPos {
+        float position;
+
+        public CustomViewPos(float position) {
+            this.position = position;
+        }
     }
 
 }
